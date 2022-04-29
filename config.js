beb7dec90816dc5bf31c2fb8861803a75d4d3dc2
Dryspell <Nhwinslow@gmail.com>
2022-04-28 21:12:09 -0400

    Made changes

--- config.js
+++ config.js
@@ -13,18 +13,18 @@ const fs = require("fs");
 
 let today = Date.now();
 today = new Date(today);
-console.log(typeof today);
-console.log(today.toLocaleDateString());
+// console.log(typeof today);
+// console.log(today.toLocaleDateString());
 
 // The last 30 days.
 const thirtyDays = new Date(today.getTime() - 30 * 24 * 60 * 60 * 1000);
-console.log(thirtyDays.toLocaleDateString());
+// console.log(thirtyDays.toLocaleDateString());
 
 // Make a loop that prints out the last 30 days.
 let last30days = [];
 for (let i = 0; i < 30; i++) {
   const date = new Date(today.getTime() - i * 24 * 60 * 60 * 1000);
-  console.log(date.toLocaleDateString());
+  // console.log(date.toLocaleDateString());
   last30days.push(Date.parse(date));
 }
 console.log(last30days);
@@ -33,29 +33,23 @@ const fetchdata = async (coinid, currency, fromdate, todate) => {
   let url = `https://api.coingecko.com/api/v3/coins/${coinid}/market_chart/range?vs_currency=${currency}&from=${parseInt(
     fromdate / 1000
   )}&to=${parseInt(todate / 1000)}`;
-  console.log(url);
-  axios
+  // console.log(url);
+  return axios
     .get(
       url
       // `https://api.coingecko.com/api/v3/coins/${coinid}/market_chart?vs_currency=usd&days=1`
     )
     .then((response) => {
-      console.log(response.data);
-      return response.data;
+      // console.log(response.data, typeof response.data);
+      documents.push(response.data);
     });
 };
-// Pull data from API
-documents = [];
-for (let i = 0; i < last30days.length; i++) {
-  fetchdata("bitcoin", "usd", last30days[i + 1], last30days[i]).then((data) => {
-    documents.push(data);
-  });
-}
-// fs.writeFile("./my.json", JSON.stringify(documents), function (err) {
-//   if (err) {
-//     console.error("Crap happens");
-//   }
-// });
+
+fs.writeFile("./my.json", JSON.stringify(documents), function (err) {
+  if (err) {
+    console.error("Crap happens");
+  }
+});
 
 const { MongoClient } = require("mongodb");
 const { appendFile } = require("fs");
@@ -67,10 +61,11 @@ async function listDatabases(client) {
 }
 
 const createMultipleListings = async (client, newListings) => {
-  const db = client.db("crytoland");
+  const db = client.db("cryptoland");
   const collection = db.collection("listings");
-  const result = await collection.insertOne(newListings);
-  console.log(result.insertedCount);
+  // console.log(newListings);
+  const result = await collection.insertMany(newListings);
+  console.log(result);
 };
 
 async function main() {
@@ -86,6 +81,17 @@ async function main() {
     await client.connect();
     // Make the appropriate DB calls
     await listDatabases(client);
+
+    try {
+      // Pull data from API
+    var documents = [];
+    for (let i = 0; i < last30days.length; i++) {
+      fetchdata("bitcoin", "usd", last30days[i + 1], last30days[i])
+      };
+    } catch (e){console.log(e)}
+    
+    console.log(documents);
+
     await createMultipleListings(client, documents);
   } catch (e) {
     console.error(e);
