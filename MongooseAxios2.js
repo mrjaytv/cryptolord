ffe01e732852d982b9141ae88913c8d10ea69070
Dryspell <Nhwinslow@gmail.com>
2022-05-11 20:02:07 -0400

    Did stuff

--- MongooseAxios2.js
+++ MongooseAxios2.js
@@ -2,106 +2,116 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 
 const MONGO_CONNECTION_STRING =
-  "mongodb+srv://cryptolord:cryptolordpass@cluster0.0tmx0.mongodb.net/cryptoland?retryWrites=true&w=majority";
+    "mongodb+srv://cryptolord:cryptolordpass@cluster0.0tmx0.mongodb.net/cryptoland?retryWrites=true&w=majority";
 
 const getData = async (coinsymb, currencySymb, todate = 0) => {
-  mongoose
-    .connect(MONGO_CONNECTION_STRING, { useNewUrlParser: true })
-    .then(() => {
-      console.log("Successfully connected to MongoDB Atlas!");
+    mongoose
+        .connect(MONGO_CONNECTION_STRING, { useNewUrlParser: true })
+        .then(() => {
+            console.log("Successfully connected to MongoDB Atlas!");
 
-      search = {
-        coinid: coinsymb,
-        currency: currencySymb,
-      };
-      if (todate != 0) {
-        search.todate = todate;
-      }
-
-      Data.findOne(search)
-        .then((result) => {
-          if (result == null) {
-            console.log("No data found in MongoDB, fetching from API instead");
+            search = {
+                coinid: coinsymb,
+                currency: currencySymb,
+            };
+            if (todate != 0) {
+                search.todate = todate;
+            }
             fetchdataAPI(coinsymb, currencySymb, todate);
-          } else {
-            console.log(
-              `Found a document result from MongoDB: ${result._id},\nQuery: ${result.coinid}, ${result.currency}, ${result.todate}`,
-              `\nNow searching for older data using fromdate: ${result.fromdate}`
-            );
-            getData(result.coinid, result.currency, result.fromdate);
-          }
-        })
-        .catch((e) => {
-          console.log("Error connecting to MongoDB Atlas... Exiting now...", e);
+
+            //     Data.findOne(search)
+            //         .then((result) => {
+            //             if (result == null) {
+            //                 console.log(
+            //                     "No data found in MongoDB, fetching from API instead"
+            //                 );
+            //                 fetchdataAPI(coinsymb, currencySymb, todate);
+            //             } else {
+            //                 console.log(
+            //                     `Found a document result from MongoDB: ${result._id},\nQuery: ${result.coinid}, ${result.currency}, ${result.todate}`,
+            //                     `\nNow searching for older data using fromdate: ${result.fromdate}`
+            //                 );
+            //                 getData(
+            //                     result.coinid,
+            //                     result.currency,
+            //                     result.fromdate
+            //                 );
+            //             }
+            //         })
+            //         .catch((e) => {
+            //             console.log(
+            //                 "Error connecting to MongoDB Atlas... Exiting now...",
+            //                 e
+            //             );
+            //         });
         });
-    });
 };
 //Call This function to get the data from the database or from the API, trying the database first
 getData("BTC", "USD");
 
 //This function is called by the server to get the data from the API and transfers it to the database
 const fetchdataAPI = async (
-  coinsymb = "BTC",
-  currencySymb = "USD",
-  todate = 0
+    coinsymb = "BTC",
+    currencySymb = "USD",
+    todate = 0
 ) => {
-  let api_key =
-    "92ea2cffe73e189909a7ad2a7a3095ebe83a9dddb6baf10c587cc0c0e5c7a58f";
-  let fetchURL = `https://min-api.cryptocompare.com/data/v2/histominute?fsym=${coinsymb}&tsym=${currencySymb}&limit=2000&api_key=${api_key}`;
+    let api_key =
+        "92ea2cffe73e189909a7ad2a7a3095ebe83a9dddb6baf10c587cc0c0e5c7a58f";
+    let fetchURL = `https://min-api.cryptocompare.com/data/v2/histominute?fsym=${coinsymb}&tsym=${currencySymb}&limit=2000&api_key=${api_key}`;
 
-  if (todate != 0) {
-    fetchURL += `&toTs=${todate}`;
-  }
+    if (todate != 0) {
+        fetchURL += `&toTs=${todate}`;
+    }
 
-  console.log(fetchURL);
+    console.log(fetchURL);
 
-  axios
-    .get(fetchURL)
-    .then(function (response) {
-      if (response.status !== 200) {
-        console.log(`Failed to find data, was returned: `, response);
-      } else {
-        return onAPIFetchSuccess(coinsymb, currencySymb, response.data);
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    axios
+        .get(fetchURL)
+        .then(function (response) {
+            if (response.status !== 200) {
+                console.log(`Failed to find data, was returned: `, response);
+            } else {
+                return onAPIFetchSuccess(coinsymb, currencySymb, response.data);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 };
 
 //Here we create the schema for the data
 var repSchema = mongoose.Schema({
-  coinid: String,
-  currency: String,
-  fromdate: Number,
-  todate: Number,
-  datakeys: Array,
-  data: Object,
-  timeinterval: Number,
+    coinid: String,
+    currency: String,
+    fromdate: Number,
+    todate: Number,
+    datakeys: Array,
+    data: Object,
+    timeinterval: Number,
 });
 var Data = mongoose.model("CryptoCompare", repSchema);
 
 function onAPIFetchSuccess(coinsymb, currencySymb, response) {
-  response = response.Data;
-  console.log(response.Data[0]);
-  var data = new Data();
-  data.data = response.Data;
-  data.datakeys = Object.keys(response.Data[0]);
-  data.coinid = coinsymb;
-  data.currency = currencySymb;
-  data.fromdate = response.TimeFrom;
-  data.todate = response.TimeTo;
-  data.timeinterval = response.Data.length;
+    response = response.Data;
+    // console.log(response.Data);
+    var data = new Data();
+    data.data = response.Data;
+    data.datakeys = Object.keys(response.Data[0]);
+    data.coinid = coinsymb;
+    data.currency = currencySymb;
+    data.fromdate = response.TimeFrom;
+    data.todate = response.TimeTo;
+    data.timeinterval = response.Data.length;
 
-  return data
-    .save()
-    .then((result) => {
-      console.log(`Inserted new documents successfully, 
+    return data
+        .save()
+        .then((result) => {
+            console.log(`Inserted new documents successfully, 
       \nQuery: ${result.coinid}, ${result.currency}, ${result.todate},
       \nnow searching for older data using fromdate: ${result.fromdate}`);
-      getData(result.coinid, result.currency, result.fromdate);
-    })
-    .catch((e) => {
-      console.log(e);
-    });
+            getData(result.coinid, result.currency, result.fromdate);
+        })
+        .catch((e) => {
+            console.log(e);
+        });
 }
--- visualization.js
+++ visualization.js
@@ -1,71 +1,86 @@
 var mongoose = require("mongoose");
-const Json2csvParser = require("json2csv").Parser;
+// const Json2csvParser = require("json2csv").Parser;
 const fs = require("fs");
-const createCsvWriter = require("csv-writer").createObjectCsvWriter;
-const fastcsv = require("fast-csv");
-const ws = fs.createWriteStream("fastcsv.csv");
+// const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+// const fastcsv = require("fast-csv");
+// const ws = fs.createWriteStream("fastcsv.csv");
 
 const MONGO_CONNECTION_STRING =
-  "mongodb+srv://cryptolord:cryptolordpass@cluster0.0tmx0.mongodb.net/cryptoland?retryWrites=true&w=majority";
+    "mongodb+srv://cryptolord:cryptolordpass@cluster0.0tmx0.mongodb.net/cryptoland?retryWrites=true&w=majority";
 
 const getData = async (coinsymb, currencySymb, todate = 0) => {
-  mongoose
-    .connect(MONGO_CONNECTION_STRING, { useNewUrlParser: true })
-    .then(() => {
-      console.log("Successfully connected to MongoDB Atlas!");
+    mongoose
+        .connect(MONGO_CONNECTION_STRING, { useNewUrlParser: true })
+        .then(() => {
+            console.log("Successfully connected to MongoDB Atlas!");
 
-      search = {
-        coinid: coinsymb,
-        currency: currencySymb,
-      };
+            search = {
+                coinid: coinsymb,
+                currency: currencySymb,
+                timeinterval: { $gte: 2001 },
+            };
 
-      Data.find(search)
-        .then((result) => {
-          // console.log(result[0]);
-          // USING JSON2CSV
-          const json2csvParser = new Json2csvParser({ header: false });
-          const csvData = json2csvParser.parse(result);
-          fs.writeFile("data.csv", csvData, (err) => {
-            if (err) throw err;
-            console.log("File written successfully!");
-          });
-          // USING CSV-WRITER
-          // const csvWriter = createCsvWriter({
-          //   path: "data.csv",
-          //   header: [
-          //     { id: "_id", title: "_id" },
-          //     { id: "high", title: "high" },
-          //     { id: "low", title: "low" },
-          //     { id: "open", title: "open" },
-          //     { id: "close", title: "close" },
-          //     { id: "time", title: "time" },
-          //     { id: "volumefrom", title: "volumefrom" },
-          //     { id: "volume", title: "volume" },
-          //     { id: "date", title: "date" },
-          //   ],
-          // });
-          // csvWriter
-          //   .writeRecords(result)
-          //   .then(() =>
-          //     console.log(
-          //       "Write to bezkoder_mongodb_csvWriter.csv successfully!"
-          //     )
-          //   );
-          // USING FAST-CSV
-          // fastcsv
-          //   .write(result, { headers: true })
-          //   .on("finish", function () {
-          //     console.log(
-          //       "Write to bezkoder_mongodb_fastcsv.csv successfully!"
-          //     );
-          //   })
-          //   .pipe(ws);
-          // return result;
-        })
-        .catch((e) => {
-          console.log("Error connecting to MongoDB Atlas... Exiting now...", e);
+            Data.find(search)
+                .then((result) => {
+                    var fs = require("fs");
+                    fs.writeFile(
+                        "test.txt",
+                        JSON.stringify(result),
+                        function (err) {
+                            if (err) {
+                                console.log(err);
+                            }
+                        }
+                    );
+
+                    // console.log(result[0]);
+                    // USING JSON2CSV
+                    // const json2csvParser = new Json2csvParser({ header: false });
+                    // const csvData = json2csvParser.parse(result);
+                    // fs.writeFile("data.csv", csvData, (err) => {
+                    //   if (err) throw err;
+                    //   console.log("File written successfully!");
+                    // });
+                    // USING CSV-WRITER
+                    // const csvWriter = createCsvWriter({
+                    //   path: "data.csv",
+                    //   header: [
+                    //     { id: "_id", title: "_id" },
+                    //     { id: "high", title: "high" },
+                    //     { id: "low", title: "low" },
+                    //     { id: "open", title: "open" },
+                    //     { id: "close", title: "close" },
+                    //     { id: "time", title: "time" },
+                    //     { id: "volumefrom", title: "volumefrom" },
+                    //     { id: "volume", title: "volume" },
+                    //     { id: "date", title: "date" },
+                    //   ],
+                    // });
+                    // csvWriter
+                    //   .writeRecords(result)
+                    //   .then(() =>
+                    //     console.log(
+                    //       "Write to bezkoder_mongodb_csvWriter.csv successfully!"
+                    //     )
+                    //   );
+                    // USING FAST-CSV
+                    // fastcsv
+                    //   .write(result, { headers: true })
+                    //   .on("finish", function () {
+                    //     console.log(
+                    //       "Write to bezkoder_mongodb_fastcsv.csv successfully!"
+                    //     );
+                    //   })
+                    //   .pipe(ws);
+                    // return result;
+                })
+                .catch((e) => {
+                    console.log(
+                        "Error connecting to MongoDB Atlas... Exiting now...",
+                        e
+                    );
+                });
         });
-    });
 };
 //Call This function to get the data from the database or from the API, trying the database first
 
@@ -73,6 +88,6 @@ getData("BTC", "USD"); //.then((result) => {
 
 //Here we create the schema for the data
 var repSchema = mongoose.Schema({
-  any: {},
+    any: {},
 });
 var Data = mongoose.model("cryptoindicator", repSchema);
